0ecb4735e60aa67884a964d00eafb989f88167a3
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-10-24 12:56:21 +0200

    add before, beforeclass

--- org.javacream.training.java/test/org/javacream/training/java/basic/GreeterTest.java
+++ org.javacream.training.java/test/org/javacream/training/java/basic/GreeterTest.java
@@ -1,19 +1,37 @@
 package org.javacream.training.java.basic;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GreeterTest
 {
 
+	private static String greeting;
+	private static String name;
+	private Greeter greeter;
+
+	@BeforeClass
+	public static void staticInit()
+	{
+		System.out.println("Before Class init...");
+		greeting = "Hello";
+		name = "Sawitzki";
+	}
+
+	
+	@Before
+	public void init()
+	{
+		System.out.println("Before init...");
+		greeter = new Greeter(greeting);
+	}
+	
 	@Test
 	public void testGreeterGreetsName()
 	{
-		final String greeting = "Hello";
-		final String name = "Sawitzki";
-		final Greeter greeter = new Greeter(greeting);
 		final String expectedGreeting = "Hello Sawitzki";
-
 		String result = greeter.greet(name);
 		Assert.assertEquals(expectedGreeting, result);
 	}
@@ -21,9 +39,6 @@ public class GreeterTest
 	@Test
 	public void testGreeterGreetsNameFriendly()
 	{
-		final String greeting = "Hello";
-		final String name = "Sawitzki";
-		final Greeter greeter = new Greeter(greeting);
 		final String expectedGreeting = "Hello Sawitzki, nice to meet you";
 		String result = greeter.greet(name, true);
 		Assert.assertEquals(expectedGreeting, result);
@@ -33,9 +48,6 @@ public class GreeterTest
 	@Test
 	public void testGreeterGreetsNameNotFriendly()
 	{
-		final String greeting = "Hello";
-		final String name = "Sawitzki";
-		final Greeter greeter = new Greeter(greeting);
 		final String expectedGreeting = "Hello Sawitzki";
 		String result = greeter.greet(name, false);
 		Assert.assertEquals(expectedGreeting, result);
