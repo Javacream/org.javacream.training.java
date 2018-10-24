103dc2c680d0ed9bef75ede585dc2d181d1457d1
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-10-24 11:35:27 +0200

    add echo add helper

---
+++ org.javacream.training.java/src/org/javacream/training/java/basic/Echo.java
@@ -0,0 +1,12 @@
+package org.javacream.training.java.basic;
+
+import org.javacream.util.Helper;
+
+public class Echo
+{
+
+	public String echo(String message)
+	{
+		throw Helper.notYetImplemented();
+	}
+}
