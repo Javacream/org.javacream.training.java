97bd07aee5089b506a1066c6c76cd11f32350cd3
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-06-08 11:52:33 +0200

    change PeopleApplication

--- org.javacream.training.java/src/org/javacream/training/java/PeopleApplication.java
+++ org.javacream.training.java/src/org/javacream/training/java/PeopleApplication.java
@@ -5,6 +5,10 @@ public class PeopleApplication {
 	public static void main(String[] args) {
 		Person sawitzki = new Person("Sawitzki", "Rainer", 80.5, 183);
 		Person mustermann = new Person("Mustermann", "Hans", 99.3, 176);
+		String lastname = sawitzki.lastname;
+		System.out.println(lastname);
+		lastname = mustermann.lastname;
+		System.out.println(lastname);
 	}
 
 }
