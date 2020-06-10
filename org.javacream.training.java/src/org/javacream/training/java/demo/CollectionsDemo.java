301252a792fa5566ae3c0615322db4d04e8f64d0
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-06-10 10:01:43 +0200

    add mapDemo

--- org.javacream.training.java/src/org/javacream/training/java/demo/CollectionsDemo.java
+++ org.javacream.training.java/src/org/javacream/training/java/demo/CollectionsDemo.java
@@ -1,8 +1,10 @@
 package org.javacream.training.java.demo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.javacream.training.java.people.Person;
@@ -14,10 +16,11 @@ public class CollectionsDemo {
 		new CollectionsDemo();
 
 	}
-	
+
 	public CollectionsDemo() {
-		//listDemo();
-		setDemo();
+		// listDemo();
+		// setDemo();
+		mapDemo();
 	}
 
 	private void listDemo() {
@@ -27,37 +30,34 @@ public class CollectionsDemo {
 		Address munich = new Address("München", "Marienplatz");
 		Address berlin = new Address("Berlin", "Alexanderplatz");
 		Address munich2 = new Address("München", "Marienplatz");
-		
-		
+
 		Person sawitzki = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
-		Person  sawitzki2 = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
+		Person sawitzki2 = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
 		Person mustermann = new Person("Mustermann", "Hans", 80.5, 183, berlin);
-		
+
 		people.add(sawitzki);
 		people.add(sawitzki2);
 		people.add(mustermann);
 		people.add(sawitzki);
-		
+
 		addresses.add(munich);
 		addresses.add(munich2);
 		addresses.add(berlin);
 		addresses.add(munich);
-		
-		//for Person p in people
-		for (Person p: people) {
+
+		// for Person p in people
+		for (Person p : people) {
 			System.out.println(p);
 		}
-		for (Address a: addresses) {
+		for (Address a : addresses) {
 			System.out.println(a);
 		}
 
 		System.out.println(people.get(1));
 		System.out.println(people.size());
 		System.out.println(addresses.size());
-		
-		
-	}
 
+	}
 
 	private void setDemo() {
 		Set<Person> people = new HashSet<>();
@@ -66,35 +66,54 @@ public class CollectionsDemo {
 		Address munich = new Address("München", "Marienplatz");
 		Address berlin = new Address("Berlin", "Alexanderplatz");
 		Address munich2 = new Address("München", "Marienplatz");
-		
-		
+
 		Person sawitzki = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
-		Person  sawitzki2 = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
+		Person sawitzki2 = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
 		Person mustermann = new Person("Mustermann", "Hans", 80.5, 183, berlin);
-		
+
 		people.add(sawitzki);
 		people.add(sawitzki2);
 		people.add(mustermann);
 		people.add(sawitzki);
-		
+
 		addresses.add(munich);
 		addresses.add(munich2);
 		addresses.add(berlin);
 		addresses.add(munich);
-		
-		//for Person p in people
-		for (Person p: people) {
+
+		// for Person p in people
+		for (Person p : people) {
 			System.out.println(p);
 		}
-		for (Address a: addresses) {
+		for (Address a : addresses) {
 			System.out.println(a);
 		}
-		
-		//System.out.println(people.get(1));
+
+		// System.out.println(people.get(1));
 		System.out.println(people.size());
 		System.out.println(addresses.size());
-		
-		
+
 	}
 
+	private void mapDemo() {
+		Map<Integer, Person> people = new HashMap<>();
+		Address munich = new Address("München", "Marienplatz");
+		Address berlin = new Address("Berlin", "Alexanderplatz");
+
+		Person sawitzki = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
+		people.put(Person.getCounter(), sawitzki);
+
+		Person sawitzki2 = new Person("Sawitzki", "Rainer", 80.5, 183, munich);
+		people.put(Person.getCounter(), sawitzki2);
+
+		Person mustermann = new Person("Mustermann", "Hans", 80.5, 183, berlin);
+		people.put(Person.getCounter(), mustermann);
+
+		// for Person p in people
+		for (Integer i : people.keySet()) {
+			System.out.println(i + "=" + people.get(i));
+		}
+		System.out.println(people.size());
+
+	}
 }
