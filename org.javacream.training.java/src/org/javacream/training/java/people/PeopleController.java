e3422d3a6a999c923905c2e3059859565884e36e
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-06-10 11:09:56 +0200

    introduce PeopleController

---
+++ org.javacream.training.java/src/org/javacream/training/java/people/PeopleController.java
@@ -0,0 +1,16 @@
+package org.javacream.training.java.people;
+
+import java.util.Set;
+
+import org.javacream.training.java.util.Address;
+
+public interface PeopleController {
+
+	Integer create(String lastname, String firstname, Double weight, Integer height, Address address);
+	Person findById(Integer id);
+	Set<Person> findByLastname(String lastname);
+	void deleteById(Integer id);
+	void updateWeight(Integer id, Double newWeight);
+	void updateLastname(Integer id, String newLastname);
+	void updateHeight(Integer id, Integer newHeight);
+}
