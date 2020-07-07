491645efcc2955ed783cef484e04dfa1236a879b
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-07 13:58:07 +0200

    introduce ui

---
+++ org.javacream.training.java/src/org/javacream/training/java/people/PeopleUi.java
@@ -0,0 +1,63 @@
+package org.javacream.training.java.people;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class PeopleUi {
+
+	public static void main(String[] args) {
+		new PeopleUi();
+	}
+
+	private JFrame frame;
+	private JButton saveButton;
+	private JTextField inputText;
+	private JLabel outputLabel;
+	
+	public PeopleUi() {
+		initFrame();
+		staticUi();
+		registerEventListener();
+	}
+	
+	private void registerEventListener() {
+		saveButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String input = inputText.getText();
+				String result = input.toUpperCase();
+				outputLabel.setText(result);
+			}
+		});
+	}
+
+	private void staticUi() {
+		Container panel = frame.getContentPane();
+		panel.setLayout(new FlowLayout());
+		
+		saveButton = new JButton("Save");
+		inputText = new JTextField("CHANGE ME");
+		outputLabel = new JLabel("Result");
+		
+		panel.add(saveButton);
+		panel.add(inputText);
+		panel.add(outputLabel);
+	}
+
+	private void initFrame() {
+		frame = new JFrame("P E O P L E");
+		frame.setBounds(100,  100,  500,  500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.pack();
+		frame.setVisible(true);
+	}
+
+}
