36ac1f24796d69687ca9fbb834e1c561b689c516
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-08-03 14:06:56 +0200

    finish jdbc scratch

--- org.javacream.training.java/test/org/javacream/training/java/demo/jdbc/SimpleJdbcTest.java
+++ org.javacream.training.java/test/org/javacream/training/java/demo/jdbc/SimpleJdbcTest.java
@@ -2,6 +2,7 @@ package org.javacream.training.java.demo.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,14 +20,37 @@ public class SimpleJdbcTest {
 		Connection con = DriverManager.getConnection(url, user, password);
 		Assert.assertNotNull(con);
 		
-		String sqlStatement = "select isbn, title, price from books";
-		Statement statement = con.createStatement();
-		ResultSet result = statement.executeQuery(sqlStatement);
-		while(result.next()) {
-			System.out.println(result.getString(3));
+		String whereClause = "%i%";
+		//VORSICHT: Statements sind Security-Anfällig! SQL-Injection
+//		String sqlStatement = "select * from books where title like '" + whereClause + "';";
+//		Statement statement = con.createStatement();
+//		ResultSet queryResult = statement.executeQuery(sqlStatement);
+//		while(queryResult.next()) {
+//			System.out.println(queryResult.getString(2));
+//		}
+		
+		String sqlPreparedStatement = "select * from books where title like ?1;";
+		PreparedStatement preparedStatement = con.prepareStatement(sqlPreparedStatement);
+		preparedStatement.setString(1, whereClause);
+		ResultSet queryResult = preparedStatement.executeQuery();
+		while(queryResult.next()) {
+			System.out.println(queryResult.getString(2));
 		}
-		result.close();
-		statement.close();
+		
+		
+		
+		
+//		String insertStatement = "insert into books (isbn, title, price, pages, available) values('ISBN6', 'Linux', 9.99, 2500, 'true');";
+//		int numberOfRows = statement.executeUpdate(insertStatement);
+//		Assert.assertEquals(1, numberOfRows);
+
+//		String deleteStatement = "delete from  books;";
+//		int numberOfRows = statement.executeUpdate(deleteStatement);
+//		Assert.assertEquals(6, numberOfRows);
+
+		
+		queryResult.close();
+		preparedStatement.close();
 		con.close();
 	}
 }
