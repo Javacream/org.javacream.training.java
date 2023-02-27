446f8edbdf000b1a1692250b437eccbb9f6c0f42
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-02-27 14:58:50 +0100

    play with functional

--- projects/src/test/java/org/javacream/training/java/functional/FunctionalDemoTest.java
+++ projects/src/test/java/org/javacream/training/java/functional/FunctionalDemoTest.java
@@ -1,8 +1,10 @@
 package org.javacream.training.java.functional;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class FunctionalDemoTest {
 
@@ -14,5 +16,25 @@ public class FunctionalDemoTest {
 
         names.forEach(s  -> System.out.println(s));
 
+    }
+    @Test public void doListTestVerbose(){
+        List<String> names = List.of("Hugo", "Emil");
+        names.forEach((String s) -> {
+            System.out.println(s);
+        });
+        Consumer<String> c = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        };
+        names.forEach(c);
+
+        Consumer<String> c2 = (s) -> System.out.println(s);
+        names.forEach(c2);
+
+        String message = "Hello";
+        System.out.println(message);
+        System.out.println("Hello");
     }
 }
