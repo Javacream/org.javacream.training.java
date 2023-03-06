6b5655071538f76d460c9af2545dd42959cc354b
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-06 12:13:08 +0100

    continue lambdas @FunctionalInterface

--- projects/Java8 Update/src/main/java/org/javacream/training/java8/lambda/Demo.java
+++ projects/Java8 Update/src/main/java/org/javacream/training/java8/lambda/Demo.java
@@ -1,6 +1,7 @@
 package org.javacream.training.java8.lambda;
 
+@FunctionalInterface
 public interface Demo {
     public void doDemo(String s);
-    public void doDemo2(String s);
+    //public void doDemo2(String s);
 }
