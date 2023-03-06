688da28f79917381f85113f07cbd4f06920ca684
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-06 15:14:25 +0100

    method references

--- projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/CollectionsTest.java
+++ projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/CollectionsTest.java
@@ -14,7 +14,24 @@ public class CollectionsTest {
         names.add("Fritz");
 
         Consumer<String> consumer = (s) -> System.out.println(s);
+        Demo d = (s) -> System.out.println(s);
+        //consumer = d; Syntaktisch nicht korrekt, obwohl exakt dieselben Ausrücke zugeweisen wurden
+        Consumer<String> consumer2 = System.out::println;
+        Demo d2 = System.out::println;
+
+        Consumer<String> consumer3 = this::demo;
+        Demo d3 = this::demo;
+
 
         names.forEach(consumer);
+        names.forEach(System.out::println);
+    }
+
+    private void demo(String s){
+        //...
+    }
+
+    interface Demo{
+        void egal(String s);
     }
 }
