b12e4b97ca0101df2669ef58a0da806b08a9b6de
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-06 12:09:04 +0100

    continue lambdas: Lambdas with 2 interface methods are impossible!

--- projects/Java8 Update/src/main/java/org/javacream/training/java8/lambda/Demo.java
+++ projects/Java8 Update/src/main/java/org/javacream/training/java8/lambda/Demo.java
@@ -2,4 +2,5 @@ package org.javacream.training.java8.lambda;
 
 public interface Demo {
     public void doDemo(String s);
+    public void doDemo2(String s);
 }
--- projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/DemoTest.java
+++ projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/DemoTest.java
@@ -11,26 +11,12 @@ public class DemoTest {
             public void doDemo(String s) {
                 System.out.println(s);
             }
+
+            @Override
+            public void doDemo2(String s) {
+
+            }
         };
-    }
-
-    @Test public void testDemoWithLambda(){
-        Demo demo = (String s) -> {
-                System.out.println(s);
-            };
-    }
-    @Test public void testDemoWithLambdaCompact(){
-        Demo demo = (String s) -> System.out.println(s);
-        Demo demo2 = s -> System.out.println(s); //Type Inference: Der Typ von s wird vom Compiler aus der Schnittstelle entnommen
-
-        //Typ-Prüfungen
-        //Demo demo3 = (Integer s) -> System.out.println(s); //Compiler Fehler, Parameter-Typ passt nicht
-        //Demo demo4 = (String s) -> return 42; //Compiler Fehler, Rückgabe-Typ passt nicht
-
-        //Syntax
-        Demo demo4 = s -> {System.out.println(s);};
-        Demo demo5 = (s) -> {System.out.println(s);};
-
     }
 
 }
