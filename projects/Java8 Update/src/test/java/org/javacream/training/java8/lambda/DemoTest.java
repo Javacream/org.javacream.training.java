e22b2cd35f26c509a3ec8796904d626d6c5dc605
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-06 11:52:43 +0100

    continue lambdas

--- projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/DemoTest.java
+++ projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/DemoTest.java
@@ -5,9 +5,32 @@ import org.junit.jupiter.api.Test;
 
 public class DemoTest {
 
-    @Test public void test(){
-        DemoImpl di = new DemoImpl();
-        di.doDemo("");
-        DemoImpl.InnerDemoImpl innerDi = di.new InnerDemoImpl();
+    @Test public void testDemoWithAnonymousClass(){
+        Demo demo = new Demo(){
+            @Override
+            public void doDemo(String s) {
+                System.out.println(s);
+            }
+        };
     }
+
+    @Test public void testDemoWithLambda(){
+        Demo demo = (String s) -> {
+                System.out.println(s);
+            };
+    }
+    @Test public void testDemoWithLambdaCompact(){
+        Demo demo = (String s) -> System.out.println(s);
+        Demo demo2 = s -> System.out.println(s); //Type Inference: Der Typ von s wird vom Compiler aus der Schnittstelle entnommen
+
+        //Typ-Prüfungen
+        //Demo demo3 = (Integer s) -> System.out.println(s); //Compiler Fehler, Parameter-Typ passt nicht
+        //Demo demo4 = (String s) -> return 42; //Compiler Fehler, Rückgabe-Typ passt nicht
+
+        //Syntax
+        Demo demo4 = s -> {System.out.println(s);};
+        Demo demo5 = (s) -> {System.out.println(s);};
+
+    }
+
 }
