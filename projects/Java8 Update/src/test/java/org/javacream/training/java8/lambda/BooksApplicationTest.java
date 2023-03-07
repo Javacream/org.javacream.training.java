9cbe25beafb3e46426a360a3d61f7ef2f0be25b8
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-07 11:44:19 +0100

    streaming

--- projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/BooksApplicationTest.java
+++ projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/BooksApplicationTest.java
@@ -1,5 +1,8 @@
 package org.javacream.training.java8.lambda;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 public class BooksApplicationTest {
 
     public void test(){
@@ -7,6 +10,8 @@ public class BooksApplicationTest {
         BooksServiceWithFinderFunctions booksServiceWithFinderFunctions = new BooksServiceWithFinderFunctions(() -> "ISBN");
         booksApplication.workWithBookListSupplier(booksServiceWithFinderFunctions.findAllSupplier);
         booksApplication.workWithBookListSupplier(booksServiceWithFinderFunctions::findAll);
+        Supplier<List<Book>> supplier =booksServiceWithFinderFunctions::findAll;
         SomethingNew sn = booksServiceWithFinderFunctions::findAll;
+        //sn = supplier;
     }
 }
--- projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/CollectionsTest.java
+++ projects/Java8 Update/src/test/java/org/javacream/training/java8/lambda/CollectionsTest.java
@@ -5,33 +5,29 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionsTest {
     @Test
-    public void testListIteration(){
+    public void testListIteration() {
         List<String> names = new ArrayList<>();
         names.add("Emil");
         names.add("Fritz");
-
-        Consumer<String> consumer = (s) -> System.out.println(s);
-        Demo d = (s) -> System.out.println(s);
-        //consumer = d; Syntaktisch nicht korrekt, obwohl exakt dieselben Ausrücke zugeweisen wurden
-        Consumer<String> consumer2 = System.out::println;
-        Demo d2 = System.out::println;
-
-        Consumer<String> consumer3 = this::demo;
-        Demo d3 = this::demo;
-
-
-        names.forEach(consumer);
-        names.forEach(System.out::println);
+        Stream<String> namesStream = names.stream();
+        defineWorkflow1(namesStream);
     }
 
-    private void demo(String s){
-        //...
+    private void defineWorkflow1(Stream<String> stream){
+        //filter - final operation = Iteration
+        Stream<String> filteredStream = stream.filter(null);
+        filteredStream.forEach(null);
     }
-
-    interface Demo{
-        void egal(String s);
+    private void defineWorkflow2(Stream<String> stream){
+        //filter - final operation = Ergebnis-Liste
+        Stream<String> filteredStream = stream.filter(null);
+//        ArrayList<String> result = new ArrayList<>();
+//        filteredStream.forEach(null); //Conusmer: result.add(element)
+        List<String> result = filteredStream.collect(Collectors.toList());
     }
 }
