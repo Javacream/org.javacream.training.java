5e1170ddebc5f4bd75b6a21e8c5a9f2a0e45d6cd
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-20 12:19:52 +0100

    junit 4.8.2

---
+++ projects/org.javacream.training/src/test/java/org/javacream/training/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package org.javacream.training.java;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld(){
+        HelloWorld hw = new HelloWorld();
+        hw.first();
+    }
+
+}
