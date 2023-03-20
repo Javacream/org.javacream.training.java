632e33077c2baa53b3326d20e2bd3da435575e98
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-20 14:29:38 +0100

    add final

--- projects/org.javacream.training/src/test/java/org/javacream/training/java/PersonTest.java
+++ projects/org.javacream.training/src/test/java/org/javacream/training/java/PersonTest.java
@@ -8,13 +8,13 @@ import static org.junit.Assert.*;
 public class PersonTest {
 
     @Test public void creatingAPersonWithLastnameSawitzkiCreatesAPersonWithLastnameSawitzki(){
-        String EXPECTED_LASTNAME = "Sawitzki";
+        final String EXPECTED_LASTNAME = "Sawitzki";
         Person p = new Person(EXPECTED_LASTNAME, "X", 183, 81.1);
         //System.out.println(p.getLastname());
         Assert.assertEquals(EXPECTED_LASTNAME, p.getLastname());
     }
     @Test public void creatingAPersonWithFirstnameRainerCreatesAPersonWithFirstnameRainer(){
-        String EXPECTED_FIRSTNAME = "Rainer";
+        final String EXPECTED_FIRSTNAME = "Rainer";
         Person p = new Person("", EXPECTED_FIRSTNAME, 183, 81.1);
         //System.out.println(p.getLastname());
         Assert.assertEquals(EXPECTED_FIRSTNAME, p.getFirstname());
@@ -22,11 +22,11 @@ public class PersonTest {
 
     @Test
     public void testIntroduce(){
-        String LASTNAME = "Sawitzki";
-        String FIRSTNAME = "Rainer";
-        Integer HEIGHT = 183;
-        Double WEIGHT = 81.1;
-        String EXPECTED_INTRODUCTION = "Person: lastname=Sawitzki, firstname=Rainer, height=183, weight=81.1";
+        final String LASTNAME = "Sawitzki";
+        final String FIRSTNAME = "Rainer";
+        final Integer HEIGHT = 183;
+        final Double WEIGHT = 81.1;
+        final String EXPECTED_INTRODUCTION = "Person: lastname=Sawitzki, firstname=Rainer, height=183, weight=81.1";
         Person p = new Person(LASTNAME, FIRSTNAME, HEIGHT, WEIGHT);
         Assert.assertEquals(EXPECTED_INTRODUCTION, p.introduce());
 
