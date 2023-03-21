96a370533d943028554b7fbbb93ee52b1329c03c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-21 14:22:22 +0100

    introduce lambda

--- projects/org.javacream.training/src/test/java/org/javacream/training/java/FunctionalTest.java
+++ projects/org.javacream.training/src/test/java/org/javacream/training/java/FunctionalTest.java
@@ -8,13 +8,9 @@ public class FunctionalTest {
 
     @Test public void testPersonIntroducer(){
         Person p = new Person("A", "B", 100, 10.0);
-        class ConsolePersonIntroducer implements PersonIntroducer{
-            @Override
-            public void introducePerson(Person p) {
-                System.out.println(p.introduce());
-            }
-        }
-        PersonIntroducer pi = new ConsolePersonIntroducer();
+        PersonIntroducer pi = (Person person) -> {
+            System.out.println(p);
+        };
         pi.introducePerson(p);
         }
     interface PersonIntroducer{
