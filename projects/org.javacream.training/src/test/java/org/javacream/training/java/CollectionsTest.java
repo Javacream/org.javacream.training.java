4974582e4c48006da23baabfda25cef837139ce8
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-21 11:21:37 +0100

    play with collections

--- projects/org.javacream.training/src/test/java/org/javacream/training/java/CollectionsTest.java
+++ projects/org.javacream.training/src/test/java/org/javacream/training/java/CollectionsTest.java
@@ -50,5 +50,67 @@ public class CollectionsTest {
 
     }
 
+    @Test public void testAddressList(){
+        Address a1 = new Address("A", "B");
+        Address a2 = new Address("C", "D");
+        Address a3 = new Address("A", "B");
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(a1);
+        addresses.add(a2);
+        addresses.add(a3);
+        Assert.assertEquals(3, addresses.size());
+        Assert.assertEquals(a1, addresses.get(0));
+        Assert.assertEquals(a2, addresses.get(1));
+        for(Address a: addresses){
+            System.out.println(a);
+        }
+    }
+    @Test public void testAddressSet(){
+        Address a1 = new Address("A", "B");
+        Address a2 = new Address("C", "D");
+        Address a3 = new Address("A", "B");
+        Set<Address> addresses = new HashSet<>();
+        addresses.add(a1);
+        addresses.add(a2);
+        addresses.add(a3);
+        Assert.assertEquals(2, addresses.size());
+        //Assert.assertEquals("Hello", addresses.get(0));
+        //Assert.assertEquals("World", addresses.get(1));
+        for(Address a: addresses){
+            System.out.println(a);
+        }
+    }
+    @Test public void testPersonList(){
+        Person p1 = new Person("A", "B", 100, 10.0);
+        Person p2 = new Person("C", "D", 100, 10.0);
+        Person p3 = new Person("A", "B", 100, 10.0);
+        List<Person> people = new ArrayList<>();
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        Assert.assertEquals(3, people.size());
+        Assert.assertEquals(p1, people.get(0));
+        Assert.assertEquals(p2, people.get(1));
+        for(Person p: people){
+            System.out.println(p);
+        }
+    }
+    @Test public void testPersonSet(){
+        Person p1 = new Person("A", "B", 100, 10.0);
+        Person p2 = new Person("C", "D", 100, 10.0);
+        Person p3 = new Person("A", "B", 100, 10.0);
+        Set<Person> people = new HashSet<>();
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        Assert.assertEquals(3, people.size());
+        //Assert.assertEquals("Hello", strings.get(0));
+        //Assert.assertEquals("World", strings.get(1));
+        for(Person p: people){
+            System.out.println(p);
+        }
+    }
+
+
 
 }
