8d3bde4159ca74d8dcf0a1084c39f42d204f41c3
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-03-21 14:59:35 +0100

    pay wit h java.util.function

--- projects/org.javacream.training/src/test/java/org/javacream/training/java/FunctionalTest.java
+++ projects/org.javacream.training/src/test/java/org/javacream/training/java/FunctionalTest.java
@@ -3,6 +3,11 @@ package org.javacream.training.java;
 import com.sun.nio.sctp.PeerAddressChangeNotification;
 import org.junit.Test;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class FunctionalTest {
 
 
@@ -13,5 +18,24 @@ public class FunctionalTest {
         }
     interface PersonIntroducer{
         public void introducePerson(Person p);
+    }
+
+    @Test public void testJavaUtilFunctions(){
+        Function<String, Integer> f1 = (String s) -> {return s.length();};
+        Function<String, Integer> f2 = (s) -> {return s.length();};
+        Function<String, Integer> f3 = s -> {return s.length();};
+        Function<String, Integer> f4 = s -> s.length();
+        Integer result = f1.apply("Hugo");
+        System.out.println(result);
+        Consumer<String> c = System.out::println;
+        c.accept("hugo");
+
+        Supplier<String> supplier = () -> {return "Hugo";};
+        String supplierResult = supplier.get();
+        System.out.println(supplierResult);
+
+        Predicate<String> predicate = (s) -> s.startsWith("H");
+        Boolean b = predicate.test("Hugo");
+        System.out.println(b);
     }
 }
