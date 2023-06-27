9bab6f52db514b4bdf88b51a6a861c32672a9604
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-06-27 09:36:53 +0200

    Test Git

--- projects/Java8 Update/src/test/java/org/javacream/training/java/setup/SimpleTest.java
+++ projects/Java8 Update/src/test/java/org/javacream/training/java/setup/SimpleTest.java
@@ -4,8 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
+
     @Test
     public void setup(){
         Assertions.assertTrue(true);
     }
+    @Test
+    public void setup2(){
+        Assertions.assertTrue(true);
+    }
+
+
 }
