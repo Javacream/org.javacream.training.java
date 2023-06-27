220f36bc34658151140448ad1be64f7556e11fce
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-06-27 11:14:47 +0200

    introduce create

--- projects/Java8 Update/src/main/java/org/javacream/people/PeopleServiceImpl.java
+++ projects/Java8 Update/src/main/java/org/javacream/people/PeopleServiceImpl.java
@@ -3,4 +3,9 @@ package org.javacream.people;
 import java.util.Objects;
 
 public class PeopleServiceImpl implements PeopleService{
+    @Override
+    public Person create(String name, Double weight) {
+        Person p = new Person(name, weight);
+        return p;
+    }
 }
