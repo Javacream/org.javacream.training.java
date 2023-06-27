e61d1026067c402a54d840a0a96ae11ce4495600
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-06-27 16:10:45 +0200

    play with functional

---
+++ projects/Java8 Update/src/main/java/org/javacream/functional/AnotherFunction.java
@@ -0,0 +1,6 @@
+package org.javacream.functional;
+
+@FunctionalInterface
+public interface AnotherFunction {
+    public boolean hugo(String p1, Integer p2);
+}
--- projects/Java8 Update/src/main/java/org/javacream/functional/MyFunctionUsage.java
+++ projects/Java8 Update/src/main/java/org/javacream/functional/MyFunctionUsage.java
@@ -2,10 +2,15 @@ package org.javacream.functional;
 
 public class MyFunctionUsage {
     public static void main(String[] args) {
-        MyFunction myFunction = (String param1, Integer param2) -> {
-            System.out.println("param1=" + param1 + ", param2=" + param2);
-            return true;
+        MyFunction myFunction = (param1, param2) -> {
+            return param1.length() > param2;
         };
+        MyFunction myFunctionTypeInference = (param1, param2) -> {
+            return param1.length() > param2;
+        };
+        MyFunction myFunctionTypeInferenceCompact = (param1, param2) -> param1.length() > param2;//Ohne Block: implizites return des letzten Ausdrucks
+        AnotherFunction af = (param1, param2) -> param1.length() > param2;
+        //MyFunction f = af; Compiler Fehler, cast ist nicht möglich
         System.out.println(myFunction.fn("Hugo", 42));
     }
 }
