aee7fcce88a619f6e519d6c80e507100d4afa716
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-06-28 12:03:04 +0200

    todo page 34

--- projects/Java8 Update/src/main/java/org/javacream/streaming/First.java
+++ projects/Java8 Update/src/main/java/org/javacream/streaming/First.java
@@ -1,6 +1,8 @@
 package org.javacream.streaming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class First {
@@ -10,7 +12,9 @@ public class First {
 
     {
         String[] names = {"Hugo", "Emil", "Fritz", "Eduard"};
+        workflow(Arrays.stream(names));
         workflowCompact(Arrays.stream(names));
+        workflowWithoutStreams(names);
     }
 
     public void workflow(Stream<String> stringStream){
@@ -21,5 +25,17 @@ public class First {
     }
     public void workflowCompact(Stream<String> stringStream){
         stringStream.filter(s -> s.startsWith("E")).map(s -> s.length()).forEach(System.out::println);
+    }
+    public void workflowWithoutStreams(String[] strings){
+        List<Integer> filteredAndTransformed = new ArrayList<>();
+        for (String s: strings){
+            if (s.startsWith("E")){
+                int length = s.length();
+                filteredAndTransformed.add(length);
+            }
+        }
+        for (Integer length: filteredAndTransformed){
+            System.out.println(length);
+        }
     }
 }
