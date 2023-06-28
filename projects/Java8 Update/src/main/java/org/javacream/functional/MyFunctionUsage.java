799f8088e38e12301ada5d412423ad9834bb9130
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-06-28 09:26:43 +0200

    introduce java.util.function

--- projects/Java8 Update/src/main/java/org/javacream/functional/MyFunctionUsage.java
+++ projects/Java8 Update/src/main/java/org/javacream/functional/MyFunctionUsage.java
@@ -1,5 +1,8 @@
 package org.javacream.functional;
 
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
 public class MyFunctionUsage {
     public static void main(String[] args) {
         MyFunction myFunction = (param1, param2) -> {
@@ -10,8 +13,21 @@ public class MyFunctionUsage {
         };
         MyFunction myFunctionTypeInferenceCompact = (param1, param2) -> param1.length() > param2;//Ohne Block: implizites return des letzten Ausdrucks
         AnotherFunction af = (param1, param2) -> param1.length() > param2;
+        BiFunction<String, Integer, Boolean> bf = (param1, param2) -> param1.length() > param2;
         //MyFunction f = af; Compiler Fehler, cast ist nicht möglich
         System.out.println(myFunction.fn("Hugo", 42));
+        System.out.println(bf.apply("Hugo", 42));
+        Predicate<String> p = (s) -> s.length() == 4;
+        p.test("Hugo");
+
+        BiFunction<String, Integer, Boolean> bf2 = new BiFunction<String, Integer, Boolean>() {
+            @Override
+            public Boolean apply(String param1, Integer param2) {
+                return param1.length() > param2;
+            }
+        };
+
+
     }
 }
 
