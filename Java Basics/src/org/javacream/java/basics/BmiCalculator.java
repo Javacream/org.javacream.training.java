036694b124be33a86488917215125aa597593a6b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 16:09:36 +0200

    finish BmiCalculator

--- Java Basics/src/org/javacream/java/basics/BmiCalculator.java
+++ Java Basics/src/org/javacream/java/basics/BmiCalculator.java
@@ -9,7 +9,17 @@ public class BmiCalculator {
 		try {
 			var pathToPersonFile = Path.of("person.txt");
 			var content = Files.readString(pathToPersonFile);
-			System.out.println(content);
+			var name = content.substring(0, 20).trim();
+			var weightString = content.substring(20, 25);
+			var heightString = content.substring(25, 28);
+			var weight = Double.parseDouble(weightString);
+			var height = Integer.parseInt(heightString)/100;
+			var bmi = weight/(height * height);
+			var result = String.format("%s has a bmi of %.2f", name, bmi);
+			System.out.println(result);
+			var pathToPersonBmiFile = Path.of("person.bmi.txt");
+			Files.writeString(pathToPersonBmiFile, result);
+			
 		}
 		catch(Exception e) {
 			System.out.println("Error, file person.txt not found or corrupt");
