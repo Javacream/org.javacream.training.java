0666c9a4c0dedaa8d43b3c04d89902275da9e978
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 15:19:34 +0200

    try catch part 2

--- Java Basics/src/org/javacream/java/basics/PersonExample.java
+++ Java Basics/src/org/javacream/java/basics/PersonExample.java
@@ -14,12 +14,19 @@ public class PersonExample {
 			var weight = Double.parseDouble(weightString);
 			System.out.print("enter your height: ");
 			var heightString = scanner.nextLine();
-			var height = Integer.parseInt(heightString);
-			System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
-			scanner.close();
-			weight += 1;
-			height -= 1;
-			System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
+			try {
+				var height = Integer.parseInt(heightString);
+				System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
+				scanner.close();
+				weight += 1;
+				height -= 1;
+				System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
+
+			}
+			catch(Exception e) {
+				System.out.println("the height input is invalid, not a number: " + heightString);
+				e.printStackTrace();
+			}
 		}
 		catch(Exception e) {
 			System.out.println("the weight input is invalid, not a number: " + weightString);
