0b77b8dce0de371c814ea24b8e7459a677331b75
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 09:30:20 +0200

    add LoopExample

---
+++ Java Basics/src/org/javacream/java/basics/LoopExample.java
@@ -0,0 +1,12 @@
+package org.javacream.java.basics;
+
+public class LoopExample {
+
+	public static void main(String[] args) {
+		for (var i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
