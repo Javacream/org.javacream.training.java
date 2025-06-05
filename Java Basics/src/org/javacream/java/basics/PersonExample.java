dde7347a6e1a110c2ce9b5b8520de6b1db197081
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 14:08:07 +0200

    person example with input

--- Java Basics/src/org/javacream/java/basics/PersonExample.java
+++ Java Basics/src/org/javacream/java/basics/PersonExample.java
@@ -1,12 +1,18 @@
 package org.javacream.java.basics;
 
+import java.util.Scanner;
+
 public class PersonExample {
 
 	public static void main(String[] args) {
-		var name = "Rainer Sawitzki";
-		var weight = 76.6;
-		var height = 183;
-		System.out.println("done");
+		var scanner = new Scanner(System.in);
+		System.out.print("enter your name: ");
+		var name = scanner.nextLine();
+		System.out.print("enter your weight: ");
+		var weight = scanner.nextLine();
+		System.out.print("enter your height: ");
+		var height = scanner.nextLine();
+		System.out.println(String.format("%s has a weight of %s and a height of %s", name, weight, height));
 	}
 
 }
