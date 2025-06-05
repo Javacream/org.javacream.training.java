c5a5abe1d21a3451b70722b0bf36df3ba4e37a82
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 16:23:26 +0200

    fix BmiCalculator

--- Java Basics/src/org/javacream/java/basics/BmiCalculator.java
+++ Java Basics/src/org/javacream/java/basics/BmiCalculator.java
@@ -12,18 +12,27 @@ public class BmiCalculator {
 			var name = content.substring(0, 20).trim();
 			var weightString = content.substring(20, 25);
 			var heightString = content.substring(25, 28);
-			var weight = Double.parseDouble(weightString);
-			var height = Integer.parseInt(heightString)/100;
-			var bmi = weight/(height * height);
-			var result = String.format("%s has a bmi of %.2f", name, bmi);
-			System.out.println(result);
-			var pathToPersonBmiFile = Path.of("person.bmi.txt");
-			Files.writeString(pathToPersonBmiFile, result);
-			
+			try {
+				var weight = Double.parseDouble(weightString);
+				var height = Double.parseDouble(heightString)/100;
+				var bmi = weight/(height * height);
+				var result = String.format("%s has a bmi of %.2f", name, bmi);
+				System.out.println(result);
+				try {
+					var pathToPersonBmiFile = Path.of("person.bmi.txt");
+					Files.writeString(pathToPersonBmiFile, result);
+	
+				}
+				catch(Exception e) {
+					System.out.println("Error, cannot write to file person.bmi.txt");
+				}
+			}
+			catch(Exception e) {
+				System.out.println(String.format("error converting numbers, height=%s, weight=%s", heightString, weightString));
+			}
 		}
 		catch(Exception e) {
 			System.out.println("Error, file person.txt not found or corrupt");
 		}
 	}
-
 }
--- README.md
+++ README.md
@@ -11,7 +11,7 @@
   * Seminarzeiten:
     * Tag 1: 10:00 - 17:00 mit Pausen 12:30 - 13:30 und 15:30-15:45
     * Tag 2: 8:30 - 16:30 mit Pausen 10:30 - 10:45, 12:15 - 13:15 und 14:45-15:00
-    * Tag 3: 8:30 - 15:30 mit Pausen 10:30 - 10:45, 12:15 - 13:15
+    * Tag 3: 8:30 - 15:30 mit Pausen 10:15 - 10:30, 12:00 - 13:00
   * Ablauf
     * Seminarbeginn 15min Warm Up, Vorstellung des Seminartages, Umgebung hochfahren, ...
     * Seminarende 30min Cool Down, Zusammenfassung, bei Bedarf Übungen fertigstellen, Fragen, Wünsche... 
