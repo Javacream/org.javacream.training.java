8464e3de1beebf9854abaa2af0bb821570b1093c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 11:15:33 +0200

    Greeter simple

---
+++ Java Basics/src/org/javacream/java/basics/Greeter.java
@@ -0,0 +1,12 @@
+package org.javacream.java.basics;
+
+public class Greeter {
+
+	public static void main(String[] args) {
+		var lastname = "Sawitzki";
+		var firstname = "Rainer";
+		var greeting = "Hello " + firstname + " " + lastname;
+		System.out.println(greeting);
+	}
+
+}
---
+++ Java Basics/src/org/javacream/java/basics/PersonExample.java
@@ -0,0 +1,12 @@
+package org.javacream.java.basics;
+
+public class PersonExample {
+
+	public static void main(String[] args) {
+		var name = "Rainer Sawitzki";
+		var weight = 76.6;
+		var height = 183;
+		System.out.println("done");
+	}
+
+}
