f1e281f73a77759bd6e6f1ce3b8dd1bb501a419c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-05 14:57:50 +0200

    try catch

--- Java Basics/src/org/javacream/java/basics/PersonExample.java
+++ Java Basics/src/org/javacream/java/basics/PersonExample.java
@@ -10,15 +10,21 @@ public class PersonExample {
 		var name = scanner.nextLine();
 		System.out.print("enter your weight: ");
 		var weightString = scanner.nextLine();
-		var weight = Double.parseDouble(weightString);
-		System.out.print("enter your height: ");
-		var heightString = scanner.nextLine();
-		var height = Integer.parseInt(heightString);
-		System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
-		scanner.close();
-		weight += 1;
-		height -= 1;
-		System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
+		try {
+			var weight = Double.parseDouble(weightString);
+			System.out.print("enter your height: ");
+			var heightString = scanner.nextLine();
+			var height = Integer.parseInt(heightString);
+			System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
+			scanner.close();
+			weight += 1;
+			height -= 1;
+			System.out.println(String.format("%s has a weight of %.2f and a height of %d", name, weight, height));
+		}
+		catch(Exception e) {
+			System.out.println("the weight input is invalid, not a number: " + weightString);
+			e.printStackTrace();
+		}
 	}
 
 }
