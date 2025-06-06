57a7d022946434257381ab4e565d4a33e318837e
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-06 10:46:14 +0200

    Listen

--- Java Basics/src/org/javacream/java/basics/BmiCalculator.java
+++ Java Basics/src/org/javacream/java/basics/BmiCalculator.java
@@ -2,6 +2,7 @@ package org.javacream.java.basics;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class BmiCalculator {
 
@@ -9,6 +10,7 @@ public class BmiCalculator {
 		try {
 			var pathToPersonFile = Path.of("people.txt");
 			var rowsOfPeople = Files.readAllLines(pathToPersonFile);
+			var resultList = new ArrayList<String>();
 			for (String rowOfPerson:  rowsOfPeople){
 				var name = rowOfPerson.substring(0, 20).trim();
 				var weightString = rowOfPerson.substring(20, 25);
@@ -19,19 +21,21 @@ public class BmiCalculator {
 					var bmi = weight/(height * height);
 					var result = String.format("%s has a bmi of %.2f", name, bmi);
 					System.out.println(result);
-					try {
-						var pathToPersonBmiFile = Path.of("people.bmi.txt");
-						Files.writeString(pathToPersonBmiFile, result);
-		
-					}
-					catch(Exception e) {
-						System.out.println("Error, cannot write to file person.bmi.txt");
-					}
+					resultList.add(result);
 				}
 				catch(Exception e) {
 					System.out.println(String.format("error converting numbers, height=%s, weight=%s", heightString, weightString));
 				}
 			}
+			try {
+				var pathToPersonBmiFile = Path.of("people.bmi.txt");
+				Files.write(pathToPersonBmiFile, resultList);
+
+			}
+			catch(Exception e) {
+				System.out.println("Error, cannot write to file person.bmi.txt");
+			}
+
 		}
 		catch(Exception e) {
 			System.out.println("Error, file person.txt not found or corrupt");
