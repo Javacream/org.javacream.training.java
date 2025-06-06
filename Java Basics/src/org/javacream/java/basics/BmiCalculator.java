f8a07c70c68d75902ef8f9573290a18dfb7e18c0
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-06-06 09:55:08 +0200

    List<String>

--- Java Basics/src/org/javacream/java/basics/BmiCalculator.java
+++ Java Basics/src/org/javacream/java/basics/BmiCalculator.java
@@ -7,28 +7,30 @@ public class BmiCalculator {
 
 	public static void main(String[] args) {
 		try {
-			var pathToPersonFile = Path.of("person.txt");
-			var content = Files.readString(pathToPersonFile);
-			var name = content.substring(0, 20).trim();
-			var weightString = content.substring(20, 25);
-			var heightString = content.substring(25, 28);
-			try {
-				var weight = Double.parseDouble(weightString);
-				var height = Double.parseDouble(heightString)/100;
-				var bmi = weight/(height * height);
-				var result = String.format("%s has a bmi of %.2f", name, bmi);
-				System.out.println(result);
+			var pathToPersonFile = Path.of("people.txt");
+			var rowsOfPeople = Files.readAllLines(pathToPersonFile);
+			for (String rowOfPerson:  rowsOfPeople){
+				var name = rowOfPerson.substring(0, 20).trim();
+				var weightString = rowOfPerson.substring(20, 25);
+				var heightString = rowOfPerson.substring(25, 28);
 				try {
-					var pathToPersonBmiFile = Path.of("person.bmi.txt");
-					Files.writeString(pathToPersonBmiFile, result);
-	
+					var weight = Double.parseDouble(weightString);
+					var height = Double.parseDouble(heightString)/100;
+					var bmi = weight/(height * height);
+					var result = String.format("%s has a bmi of %.2f", name, bmi);
+					System.out.println(result);
+					try {
+						var pathToPersonBmiFile = Path.of("people.bmi.txt");
+						Files.writeString(pathToPersonBmiFile, result);
+		
+					}
+					catch(Exception e) {
+						System.out.println("Error, cannot write to file person.bmi.txt");
+					}
 				}
 				catch(Exception e) {
-					System.out.println("Error, cannot write to file person.bmi.txt");
+					System.out.println(String.format("error converting numbers, height=%s, weight=%s", heightString, weightString));
 				}
-			}
-			catch(Exception e) {
-				System.out.println(String.format("error converting numbers, height=%s, weight=%s", heightString, weightString));
 			}
 		}
 		catch(Exception e) {
