81770f638394d324c9df18588d64d6fa42686e97
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-03 14:27:34 +0200

    Address

---
+++ Java OOP/src/org/javacream/oop/Address.java
@@ -0,0 +1,11 @@
+package org.javacream.oop;
+
+public class Address {
+	String city;
+	String street;
+	
+	Address(String city, String street){
+		this.city = city;
+		this.street = street;
+	}
+}
--- Java OOP/src/org/javacream/oop/PeopleApp.java
+++ Java OOP/src/org/javacream/oop/PeopleApp.java
@@ -4,7 +4,13 @@ public class PeopleApp {
 
 	public static void main(String[] args) {
 		var person1 = new Person("Sawitzki", "Rainer", 183);
+		person1.weight = 76.6;
 		var person2 = new Person("Musterperson", "Hannah", 198);
+		person2.weight = 79.6;
+		
+		var address1 = new Address("München", "Marienplatz");
+		var address2 = new Address("Berlin", "Alexanderplatz");
+		
 		System.out.println("done");
 
 	}
