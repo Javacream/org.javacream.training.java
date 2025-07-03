1e169f35550cd308345c85aeda5c7751775a534c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-03 11:00:29 +0200

    add Demo

---
+++ Java OOP/src/org/javacream/oop/Demo.java
@@ -0,0 +1,14 @@
+package org.javacream.oop;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		var message = "Hello";
+		var number = 9;
+		var upperCaseMessage = message.toUpperCase();
+		System.out.println(message);
+		System.out.println(upperCaseMessage);
+		
+	}
+
+}
