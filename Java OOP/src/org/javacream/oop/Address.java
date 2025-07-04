35531521c677b08428fdcfc386b664cefb568b95
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-04 09:05:50 +0200

    plain Address record

--- Java OOP/src/org/javacream/oop/Address.java
+++ Java OOP/src/org/javacream/oop/Address.java
@@ -1,12 +1,5 @@
 package org.javacream.oop;
 
 
-public record Address(String city, String street) {
-	public Address (String street) {
-		this("München", street);
-	}
-	public String info() {
-		return "Address(city=" + this.city + ", street=" + this.street + ")"; 
-	}
-}
+public record Address(String city, String street) {}
 
