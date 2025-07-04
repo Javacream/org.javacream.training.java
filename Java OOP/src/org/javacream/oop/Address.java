9a3f880cb5c7ea936c7816aa2cb571e45c79a57b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-04 09:05:20 +0200

    play with records

--- Java OOP/src/org/javacream/oop/Address.java
+++ Java OOP/src/org/javacream/oop/Address.java
@@ -1,5 +1,12 @@
 package org.javacream.oop;
 
 
-public record Address(String city, String street) {}
+public record Address(String city, String street) {
+	public Address (String street) {
+		this("München", street);
+	}
+	public String info() {
+		return "Address(city=" + this.city + ", street=" + this.street + ")"; 
+	}
+}
 
