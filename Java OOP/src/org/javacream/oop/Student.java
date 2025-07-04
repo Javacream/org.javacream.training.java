66a33044dc92bb2919ba3643c6b27e00399daa88
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-04 09:23:10 +0200

    start Student

---
+++ Java OOP/src/org/javacream/oop/Student.java
@@ -0,0 +1,9 @@
+package org.javacream.oop;
+
+public class Student extends Person {
+
+	public Student(String lastname, String firstname, Integer height) {
+		super(lastname, firstname, height);
+	}
+
+}
