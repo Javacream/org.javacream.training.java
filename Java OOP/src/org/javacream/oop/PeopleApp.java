482fb41b5506e8edbdfe792e8a58cd6c263121b7
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-04 09:33:48 +0200

    finish Student

--- Java OOP/src/org/javacream/oop/PeopleApp.java
+++ Java OOP/src/org/javacream/oop/PeopleApp.java
@@ -8,7 +8,7 @@ public class PeopleApp {
 		var address3 = new Address("Berlin", "Alexanderplatz");
 		var person1 = new Person("Sawitzki", "Rainer", 183);
 		person1.setWeight(76.6);
-		var person2 = new Person("Musterperson", "Hannah", 198);
+		var person2 = new Student("Musterperson", "Hannah", 198, "LMU");
 		person2.setWeight(79.6);
 		person1.addAddress(address1);
 		person1.addAddress(address2);
@@ -17,6 +17,7 @@ public class PeopleApp {
 		
 		System.out.println(person1.greet());
 		System.out.println(person2.info());
+		System.out.println(person2.study());
 		System.out.println(Person.getPeopleCounter());
 		System.out.println("done");
 
--- Java OOP/src/org/javacream/oop/Student.java
+++ Java OOP/src/org/javacream/oop/Student.java
@@ -1,9 +1,14 @@
 package org.javacream.oop;
 
 public class Student extends Person {
-
-	public Student(String lastname, String firstname, Integer height) {
+	private String university;
+	public Student(String lastname, String firstname, Integer height, String university) {
 		super(lastname, firstname, height);
+		this.university = university;
+	}
+	
+	public String study() {
+		return this.greet() + ", i study at " + this.university;
 	}
 
 }
