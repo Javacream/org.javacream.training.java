8aea4a2e3e64beac4605ac394eb9b4a9151e13fe
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-04 09:02:18 +0200

    introduce record

--- Java OOP/src/org/javacream/oop/Address.java
+++ Java OOP/src/org/javacream/oop/Address.java
@@ -1,38 +1,5 @@
 package org.javacream.oop;
 
-import java.util.Objects;
 
-public class Address {
-	private String city;
-	private String street;
-	
-	public Address(String city, String street){
-		this.city = city;
-		this.street = street;
-	}
+public record Address(String city, String street) {}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(city, street);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Address other = (Address) obj;
-		return Objects.equals(city, other.city) && Objects.equals(street, other.street);
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-}
